3731e987ebafaf151ce7d51bc748a4f756bd45e3
OusB2 <ousmanebamba966@gmail.com>
2026-04-24 18:25:12 +0000

    Derriere version exercice2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Déclaration des outils nécessaires
+        Scanner lecture = new Scanner(System.in);
+        int nombreSecret = (int) (Math.random() * 100) + 1;
+        boolean victoire = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("J'ai choisi un nombre entre 1 et 100.");
+        System.out.println("Vous avez 10 essais pour le trouver !");
+
+        // La boucle pour les 10 essais
+        for (int essai = 1; essai <= 10; essai++) {
+            System.out.println("Essai n°" + essai + ". Entrez un nombre :");
+            int choix = lecture.nextInt();
+
+            if (choix < nombreSecret) {
+                System.out.println("Le nombre à deviner est plus GRAND.");
+            } else if (choix > nombreSecret) {
+                System.out.println("Le nombre à deviner est plus PETIT.");
+            } else {
+                // Si on trouve le bon nombre
+                victoire = true;
+                break;
+            }
+        }
+
+        if (victoire) {
+            System.out.println("Félicitations ! Vous avez gagné.");
+        } else {
+            System.out.println("Dommage, vous avez perdu. Le nombre était : " + nombreSecret);
         }
     }
 }
